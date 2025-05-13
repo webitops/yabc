2daff0d01738a81f07250c48982cd234de9a5ec9
mouhcinemahfoud <mouhcinemahfoud@gmail.com>
2025-05-13 16:57:10 +0100

    Add CLI flags for port and transaction input in standalone wallet

--- standalone_wallet/main.go
+++ standalone_wallet/main.go
@@ -1,21 +1,33 @@
 package main
 
 import (
+	"flag"
 	"net"
 	"os"
 	"path/filepath"
-	"time"
 )
 
 func main() {
-	socketPath := filepath.Join(os.TempDir(), "yabc_wallet_127.0.0.1:7071.sock")
+	socketPort := flag.String("port", "7071", "The port to listen on for tcp socket requests.")
+	transaction := flag.String("tx", "<empty-transaction>", "The transaction to send.")
+	flag.Parse()
+
+	socketPath := filepath.Join(os.TempDir(), "yabc_wallet_127.0.0.1:"+*socketPort+".sock")
 
 	conn, err := net.Dial("unix", socketPath)
-	defer conn.Close()
+	defer func(conn net.Conn) {
+		err := conn.Close()
+		if err != nil {
+
+		}
+	}(conn)
 
 	if err != nil {
 		panic(err)
 	}
 
-	conn.Write([]byte("transaction sent: " + time.Now().String()))
+	_, err = conn.Write([]byte(*transaction))
+	if err != nil {
+		panic(err)
+	}
 }

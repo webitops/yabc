aac1b26f220e2a04c1f20549dbed33bf4bad5856
mouhcinemahfoud <mouhcinemahfoud@gmail.com>
2025-05-13 18:15:52 +0100

    Refactor wallet socket handling into a dedicated package

--- main.go
+++ main.go
@@ -3,12 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io"
-	"log"
-	"net"
-	"os"
-	"path/filepath"
 	"yabc/blockchain"
+	"yabc/wallet_socket"
 )
 
 func main() {
@@ -25,49 +21,8 @@ func main() {
 
 	go bc.Start()
 
-	// handle wallet requests
-	go func() {
-		fmt.Println("Listening for wallet requests...")
-
-		socketPath := filepath.Join(os.TempDir(), "yabc_wallet_"+bc.GetNodeAddress()+".sock")
-
-		fmt.Println("Socket path: ", socketPath)
-		if err := os.Remove(socketPath); err != nil && !os.IsNotExist(err) {
-			log.Printf("Error removing existing socket file: %v", err)
-		}
-
-		ln, err := net.Listen("unix", socketPath)
-		if err != nil {
-			log.Printf("Error listening on socket: %v", err)
-			return
-		}
-
-		for {
-			conn, err := ln.Accept()
-
-			if err != nil {
-				log.Println("Error accepting connection: ", err)
-			}
-
-			go func(conn net.Conn) {
-				defer func(conn net.Conn) {
-					err := conn.Close()
-					if err != nil {
-
-					}
-				}(conn)
-
-				message, err := io.ReadAll(conn)
-
-				if err != nil {
-					log.Println("Error reading from connection: ", err)
-				}
-
-				bc.BroadcastTransaction(string(message))
-			}(conn)
-		}
-
-	}()
+	ws := wallet_socket.NewWalletSocket(*bc)
+	go ws.Listen()
 
 	// Wait.
 	wait := make(chan bool)
---
+++ wallet_socket/wallet_socket.go
@@ -0,0 +1,65 @@
+package wallet_socket
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+	"path/filepath"
+	"yabc/blockchain"
+)
+
+type WalletSocket struct {
+	blockchain *blockchain.Blockchain
+}
+
+func NewWalletSocket(blockchain blockchain.Blockchain) *WalletSocket {
+	return &WalletSocket{
+		blockchain: &blockchain,
+	}
+}
+
+func (s *WalletSocket) Listen() {
+	fmt.Println("Listening for wallet requests...")
+
+	socketPath := filepath.Join(os.TempDir(), "yabc_wallet_"+s.blockchain.GetNodeAddress()+".sock")
+
+	fmt.Println("Socket path: ", socketPath)
+	if err := os.Remove(socketPath); err != nil && !os.IsNotExist(err) {
+		log.Printf("Error removing existing socket file: %v", err)
+	}
+
+	ln, err := net.Listen("unix", socketPath)
+	if err != nil {
+		log.Printf("Error listening on socket: %v", err)
+		return
+	}
+
+	for {
+		conn, err := ln.Accept()
+
+		if err != nil {
+			log.Println("Error accepting connection: ", err)
+		}
+
+		go s.handleWalletRequest(conn)
+	}
+}
+
+func (s *WalletSocket) handleWalletRequest(conn net.Conn) {
+	defer func(conn net.Conn) {
+		err := conn.Close()
+		if err != nil {
+
+		}
+	}(conn)
+
+	message, err := io.ReadAll(conn)
+
+	if err != nil {
+		log.Println("Error reading from connection: ", err)
+	}
+
+	s.blockchain.BroadcastTransaction(string(message))
+}
